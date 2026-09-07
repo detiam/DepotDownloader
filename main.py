28ea78c676292214906e1e3e1f1e063fa71da8be
detiam <dehe_tian@outlook.com>
2026-09-07 18:12:23 +0800

    Use compression.zstd instead zstandard

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from threading import RLock as Lock
 from urllib3.util import parse_url
 from requests.adapters import HTTPAdapter
 from concurrent.futures import ThreadPoolExecutor, Future, wait
-from zstandard import decompress as ZSTD_uncompress
+from compression.zstd import decompress as ZSTD_uncompress
 
 from steam.utils.web import make_requests_session, APIHost, DEFAULT_PARAMS
 
